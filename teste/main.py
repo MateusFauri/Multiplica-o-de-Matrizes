256936b5335406431317fae4c4c68702d14f61bb
MateusFauri <62532715+MateusFauri@users.noreply.github.com>
2024-02-14 16:44:53 -0300

    Update main.py

--- teste/main.py
+++ teste/main.py
@@ -1,33 +1,33 @@
-TAMANHO_MEDIO 4
-TAMANHO_PEQUENO 2
-MEDIA 16
+TAMANHO_MEDIO = 4
+TAMANHO_PEQUENO = 2
+MEDIA = 16
 
-matriz_A = [[1,2,3,4], 
-            [-1,-2,-3,-4], 
-            [1,2,3,4],
-            [-1,-2,-3,-4]]
+matriz_A = [[11,5,7,2],
+            [-1,5,7,-4],
+            [3,4,4,4],
+            [4,7,-6,2]]
 
-matriz_B = [[5,5,5,5],
-            [-5,-6,-7,-8],
-            [5,5,5,5],
-            [-5,-6,-7,-8]]
+matriz_B = [[5,4,8,9],
+            [-2,-10,4,9],
+            [5,0,5,0],
+            [1,1,-1,1]]
 
-matriz_C = [[1,2,3,4],
-            [-2,-2,-2,-2],
-            [1,2,3,4],
-            [-2,-2,-2,-2]]
+matriz_C = [[4,4,5,5],
+            [-3,4,1,0],
+            [1,-3,9,2],
+            [-5,-5,-5,5]]
 
-matriz_D = [[5,6,7,8],
-            [-2,-2,-2,-2],
-            [5,6,7,8],
-            [-2,-2,-2,-2]]
+matriz_D = [[-3,3,3,12],
+            [-2,-7,-3,-2],
+            [14,-12,13,3],
+            [-5,5,5,5]]
 
-matriz_b = [[1,-1], [1,-1]]
+matriz_b = [[1,-1],[1,-1]]
 matriz_m = [[0,0], [0,0]]
 resultado = [[0,0], [0,0]]
 
 
-def media_matriz(matriz[TAMANHO_MEDIO][TAMANHO_MEDIO]):
+def media_matriz(matriz):
     media = 0
 
     for row in range(0, TAMANHO_MEDIO):
@@ -37,10 +37,10 @@ def media_matriz(matriz[TAMANHO_MEDIO][TAMANHO_MEDIO]):
     return media / MEDIA
 
 def multiplicacao_matriz(matrizA, matrizB):
-    for row in range(0, TAMANHO_PEQUENO):
-        for col in range(0, TAMANHO_PEQUENO):
-            for num in range(0, TAMANHO_PEQUENO):
-                resultado[row][col] += matrizA[row][num] * matrizB[num][col]
+    resultado[0][0] = (matrizA[0][0] * matrizB[0][0]) + (matrizA[0][1] * matrizB[1][0]) 
+    resultado[0][1] = (matrizA[0][0] * matrizB[0][1]) + (matrizA[0][1] * matrizB[1][1]) 
+    resultado[1][0] = (matrizA[1][0] * matrizB[0][0]) + (matrizA[1][1] * matrizB[1][0]) 
+    resultado[1][1] = (matrizA[1][0] * matrizB[0][1]) + (matrizA[1][1] * matrizB[1][1]) 
 
     return resultado
 
@@ -49,7 +49,10 @@ matriz_m[0][0] = media_matriz(matriz_A)
 matriz_m[0][1] = media_matriz(matriz_B)
 matriz_m[1][0] = media_matriz(matriz_C)
 matriz_m[1][1] = media_matriz(matriz_D)
+print("Matriz M: ")
+print(matriz_m)
+print("\nMatriz Final:")
+resultado = multiplicacao_matriz(matriz_m, matriz_b)
+print(resultado)
 
-resultado = multiplicacao_matriz(matriz)
-print()
 
